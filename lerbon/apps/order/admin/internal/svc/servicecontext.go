2d37141246f167af748fd5ce774d48c7fc02d66c
fansg666 <2680315178@qq.com>
2024-04-18 09:17:36 +0800

    完成order创建

---
+++ lerbon/apps/order/admin/internal/svc/servicecontext.go
@@ -0,0 +1,15 @@
+package svc
+
+import (
+	"go-zeroProject/lerbon/apps/order/admin/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
